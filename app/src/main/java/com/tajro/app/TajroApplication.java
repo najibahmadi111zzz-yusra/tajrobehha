2594e42c7d8396599c752f3131cf75cf00cdbaf8
najibahmadi111zzz-yusra <najibahmadi111zzz@gmail.com>
2026-09-11 20:00:26 +0430

    Create TajroApplication.java

---
+++ app/src/main/java/com/tajro/app/TajroApplication.java
@@ -0,0 +1,24 @@
+package com.tajro.app;
+
+import android.app.Application;
+
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.appcheck.FirebaseAppCheck;
+import com.google.firebase.appcheck.debug.DebugAppCheckProviderFactory;
+
+public class TajroApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        FirebaseApp.initializeApp(this);
+
+        FirebaseAppCheck firebaseAppCheck =
+                FirebaseAppCheck.getInstance();
+
+        firebaseAppCheck.installAppCheckProviderFactory(
+                DebugAppCheckProviderFactory.getInstance()
+        );
+    }
+}
