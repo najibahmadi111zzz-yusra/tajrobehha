695b657f04ea7af46ea29f835075452c2715a842
najibahmadi111zzz-yusra <najibahmadi111zzz@gmail.com>
2026-09-15 00:14:47 +0430

    Update TajroApplication.java

--- app/src/main/java/com/tajro/app/TajroApplication.java
+++ app/src/main/java/com/tajro/app/TajroApplication.java
@@ -15,7 +15,7 @@ import com.unity3d.ads.UnityAdsError;
 
 public class TajroApplication extends Application {
 
-    private static final String UNITY_GAME_ID = "77667888";
+    private static final String UNITY_GAME_ID = "800372948";
 
     // در مرحله تست حتماً true باشد
     private static final boolean TEST_MODE = true;
