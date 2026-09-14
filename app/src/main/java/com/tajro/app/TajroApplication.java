21a50bec24e72e83273b6902c61bfcfb4d310c05
najibahmadi111zzz-yusra <najibahmadi111zzz@gmail.com>
2026-09-14 08:04:19 +0430

    Update TajroApplication.java

--- app/src/main/java/com/tajro/app/TajroApplication.java
+++ app/src/main/java/com/tajro/app/TajroApplication.java
@@ -6,19 +6,38 @@ import com.google.firebase.FirebaseApp;
 import com.google.firebase.appcheck.FirebaseAppCheck;
 import com.google.firebase.appcheck.debug.DebugAppCheckProviderFactory;
 
+import com.unity3d.ads.UnityAds;
+
 public class TajroApplication extends Application {
+
+    private static final String UNITY_GAME_ID = "800372948";
+
+    // حالت آزمایشی برای جلوگیری از نمایش تبلیغ واقعی هنگام توسعه
+    private static final boolean TEST_MODE = true;
 
     @Override
     public void onCreate() {
         super.onCreate();
 
+        // Firebase
         FirebaseApp.initializeApp(this);
 
+        // Firebase App Check
         FirebaseAppCheck firebaseAppCheck =
                 FirebaseAppCheck.getInstance();
 
         firebaseAppCheck.installAppCheckProviderFactory(
                 DebugAppCheckProviderFactory.getInstance()
+        );
+
+        // Unity Ads
+        UnityAds.initialize(
+                this,
+                UNITY_GAME_ID,
+                TEST_MODE,
+                state -> {
+                    // Unity Ads initialized
+                }
         );
     }
 }
