3b97fc62d60c01bbc86eac1ad86ddd6189f2cdce
najibahmadi111zzz-yusra <najibahmadi111zzz@gmail.com>
2026-09-14 08:10:46 +0430

    Update TajroApplication.java

--- app/src/main/java/com/tajro/app/TajroApplication.java
+++ app/src/main/java/com/tajro/app/TajroApplication.java
@@ -1,18 +1,21 @@
 package com.tajro.app;
 
 import android.app.Application;
+import android.util.Log;
 
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.appcheck.FirebaseAppCheck;
 import com.google.firebase.appcheck.debug.DebugAppCheckProviderFactory;
 
+import com.unity3d.ads.IUnityAdsInitializationListener;
 import com.unity3d.ads.UnityAds;
 
-public class TajroApplication extends Application {
+public class TajroApplication extends Application
+        implements IUnityAdsInitializationListener {
 
     private static final String UNITY_GAME_ID = "800372948";
 
-    // حالت آزمایشی برای جلوگیری از نمایش تبلیغ واقعی هنگام توسعه
+    // فعلاً حتماً true باشد؛ برای تست تبلیغ
     private static final boolean TEST_MODE = true;
 
     @Override
@@ -35,9 +38,24 @@ public class TajroApplication extends Application {
                 this,
                 UNITY_GAME_ID,
                 TEST_MODE,
-                state -> {
-                    // Unity Ads initialized
-                }
+                this
+        );
+    }
+
+    @Override
+    public void onInitializationComplete() {
+        Log.d("UnityAds", "Unity Ads initialized successfully");
+    }
+
+    @Override
+    public void onInitializationFailed(
+            UnityAds.UnityAdsInitializationError error,
+            String message) {
+
+        Log.e(
+                "UnityAds",
+                "Unity Ads initialization failed: "
+                        + error + " - " + message
         );
     }
 }
