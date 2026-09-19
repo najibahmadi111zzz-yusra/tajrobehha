3e9f767f6caa81c86c4a1a2060c3b49f66a5c32a
najibahmadi111zzz-yusra <najibahmadi111zzz@gmail.com>
2026-09-19 12:25:37 +0430

    Update ChatActivity.java

--- app/src/main/java/com/tajro/app/ChatActivity.java
+++ app/src/main/java/com/tajro/app/ChatActivity.java
@@ -46,7 +46,7 @@ public class ChatActivity extends Activity {
      * کلید واقعی را داخل چت ارسال نکن.
      */
     private static final String SUPABASE_PUBLISHABLE_KEY = 
-    "sb_publishable_a02sM3MABB4afGU90ZBdFA_OTYG6gUs";
+    "s";
 
     private static final String MEDIA_BUCKET = "chat_media";
     private static final String VOICE_BUCKET = "voice_messages";
