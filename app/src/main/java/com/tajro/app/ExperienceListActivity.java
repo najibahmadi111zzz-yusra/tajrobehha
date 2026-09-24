1fef429b0fb4238aa8509887fa9b81ed1e84dd1a
najibahmadi111zzz-yusra <najibahmadi111zzz@gmail.com>
2026-09-24 13:01:02 +0430

    Update ExperienceListActivity.java

--- app/src/main/java/com/tajro/app/ExperienceListActivity.java
+++ app/src/main/java/com/tajro/app/ExperienceListActivity.java
@@ -24,7 +24,7 @@ import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.Query;
 import com.google.firebase.firestore.DocumentSnapshot;
-import com.google.firebase.firestore.Timestamp;
+import com.google.firebase.Timestamp;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.ListenerRegistration;
 import com.google.firebase.firestore.FieldValue;
